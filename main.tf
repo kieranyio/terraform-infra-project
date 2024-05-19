402d4948e37d51eb356531ddb7b40be3d375edff
Kieran Yio <yio.kieran@gmail.com>
2024-05-19 11:54:12 +0800

    bump and add vpc endpoint

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 module "vpc" {
   source  = "app.terraform.io/kieranyio/vpc/aws"
-  version = "0.0.3"
+  version = "0.0.4"
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.${var.aws_region}.s3"
 }
