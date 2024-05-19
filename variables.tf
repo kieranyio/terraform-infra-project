4558768f747b0be45604ae71d85a2b20497b58ac
Kieran Yio <yio.kieran@gmail.com>
2024-05-19 14:40:32 +0800

    fix

--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "aws_region" {
   type    = string
   default = "ap-southeast-1"
 }
+
+variable "tfc_aws_dynamic_credentials" {
+  description = "Object containing AWS dynamic credentials configuration"
+  type = object({
+    default = object({
+      shared_config_file = string
+    })
+    aliases = map(object({
+      shared_config_file = string
+    }))
+  })
+}
--- versions.tf
+++ versions.tf
@@ -8,10 +8,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region              = "ap-southeast-1"
+  shared_config_files = [var.tfc_aws_dynamic_credentials.default.shared_config_file]
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
-  alias  = "ALIAS1"
+  region              = "ap-southeast-1"
+  alias               = "ALIAS1"
+  shared_config_files = [var.tfc_aws_dynamic_credentials.aliases["ALIAS1"].shared_config_file]
 }
